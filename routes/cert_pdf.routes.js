0193d09364fc1ff8280b0ad37a4aa091f47eeb12
nathakritbc <nathakritbc602@gmail.com>
2022-06-10 11:49:51 +0700

    first commit

---
+++ routes/cert_pdf.routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  const { full_name, course_name } = req.query;
+  res.render("cert_pdf", {
+    full_name,
+    course_name,
+  });
+});
+
+// router.post("/", async (req, res, next) => {
+//   const { full_name, course_name } = req.body;
+//   res.render("cert_pdf", {
+//     full_name,
+//     course_name,
+//   });
+// });
+
+router.post("/", async (req, res, next) => {
+  res.render("cert_pdf", { title: "Express" });
+});
+
+module.exports = router;
